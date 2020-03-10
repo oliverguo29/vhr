550401bc017cd6ca2c68003a95d569f59cca8c6a
oliver <oliverguo29@gmail.com>
2020-03-11 00:26:39 +0800

    前端登陆事件

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-       <el-form :rules="rules" :model="loginForm" class="loginContainer">
+       <el-form :rules="rules" ref="loginForm" :model="loginForm" class="loginContainer">
            <h3 class="loginTitle">系统登陆</h3>
            <el-form-item prop="username">
                <el-input type="text" v-model="loginForm.username" auto-complete="off" placeholder="请输入用户名"></el-input>
@@ -9,7 +9,7 @@
                <el-input type="text" v-model="loginForm.password" auto-complete="off" placeholder="请输入密码"></el-input>
            </el-form-item>
            <el-checkbox class="loginRemember" v-model="checked">记住密码</el-checkbox>
-           <el-button type="primary" style="width:100%">登陆</el-button>
+           <el-button type="primary" style="width:100%" @click="submitLogin">登陆</el-button>
        </el-form>
     </div>
 </template>
@@ -31,6 +31,20 @@
                 }
 
             }
+        },
+        methods:{
+            submitLogin(){
+               this.$refs.loginForm.validate((valid) =>{        //获取所有的ref
+                   if(valid){
+                       alert('submit!');
+                   } else {
+                       console.log('error submit');
+                       this.$message.error('请输入用户名密码');
+                       return false;
+                   }
+               })
+            }
+
         }
     }
 </script>
